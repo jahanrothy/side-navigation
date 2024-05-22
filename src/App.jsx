d8fef194f62fad554dd1424cf7058b9a716043e7
jahanrothy <jahanrothy58@gmail.com>
2024-05-22 13:25:15 +0600

    Responsive sidenavbar with reactjs

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import {Button, Layout, theme } from "antd";
+import {MenuUnfoldOutlined, MenuFoldOutlined} from '@ant-design/icons';
+import Logo from './components/logo';
+import MenuList from './components/MenuList';
+import ToggleThemeButton from './components/ToggleThemeButton';
+
+
+const {Header,  Sider } = Layout;
+function App() {
+  const [darkTheme, setDarkTheme] = useState(true);
+  const [collapsed, setcollapsed] = useState(false);
+
+    const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  const {
+  token: {colorBgContainer},
+  
+} = theme.useToken()
+
+  return (
+
+    <Layout>
+      <Sider
+      collapsed={collapsed} 
+      collapsible trigger={null}
+      theme={darkTheme ? 'dark' : 'light'}
+       className="sidebar">
+
+        <Logo/>
+        <MenuList darkTheme={darkTheme}/>
+        <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme}/>
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button 
+          type="text"
+          className="toggle"
+          onClick={() =>setcollapsed(!collapsed)}
+           icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}/>
+        </Header>
+      </Layout>
+    </Layout>
+   
+  );
+}
+
+export default App
